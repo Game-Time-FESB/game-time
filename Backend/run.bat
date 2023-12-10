78a6b7a614c93664bb05bab00bf69e17b74729e9
mjospovich <martin.josipovic2@gmail.com>
2023-12-10 18:10:00 +0100

    added bat file for running the server while testing

---
+++ Backend/run.bat
@@ -0,0 +1,8 @@
+@echo off
+:: To start it just right click on 'run.bat' and click 'Run Code'
+:: Runs the server in seperate terminal window
+:: Ctrl+c to quit the server
+:: 
+::
+:: api startup command, logs into terminal for now
+start cmd /k uvicorn main:app --host localhost --port 8000 --reload 2>&1
