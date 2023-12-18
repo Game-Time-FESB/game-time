5eba79c83739e3c9383b2b28cdf01b53ee2c89ba
mjospovich <martin.josipovic2@gmail.com>
2023-12-18 11:35:25 +0100

    added exception catching to network functions
    also changed log-in to sign-in to match the URL naming scheme

--- Backend/main.py
+++ Backend/main.py
@@ -27,7 +27,7 @@ class User(BaseModel):
 
 
 # * Login class definition with Pydantic BaseModel
-class Login(BaseModel):
+class SignIn(BaseModel):
     username: str  # Username for login
     password: str  # Password for login
 
@@ -46,16 +46,30 @@ def get_item():
     return {"Status": "OK"}
 
 
-# * Endpoint for logging in
-@app.post("/log-in")
-def log_in(login: Login):
-    # Checking if the provided credentials match any user in the data
-    for user in data.values():
-        if user["username"] == login.username and user["password"] == login.password:
-            return {"Log-in": "SUCCESS"}
+# * Endpoint for signing in
+@app.post("/sign-in")
+def sign_in(sign_in: SignIn):
+    try:
+        # Flag for existing username, used to be more precise in fail description
+        username_flag = False
+        # Checking if the provided credentials match any user in the data
+        for user in data.values():
+            username_flag = user["username"] == sign_in.username
 
-    # If no match is found, return failed log-in
-    return {"Log-in": "FAILED", "Info": "bad credentials"}
+            if username_flag and user["password"] == sign_in.password:
+                return {"Log-in": "SUCCESS"}
+
+        # If no match is found, return failed log-in depending on scenario
+        if username_flag:
+            return {"Log-in": "FAILED", "Info": "wrong password"}
+        else:
+            return {"Log-in": "FAILED", "Info": "username doesn't exist"}
+
+    except Exception as e:
+        # prints the error out
+        print({"Sign-in ERROR": f"{e}"})
+        # If there's an error, return sign-in error to front
+        return {"Sign-in": "ERROR"}
 
 
 # * Endpoint for signing up
@@ -84,31 +98,39 @@ def sign_up(user: User):
 
     except Exception as e:
         # prints the error out
-        print(e)
-        # If there's an error, return sign-up error
+        print({"Sign-up ERROR": f"{e}"})
+        # If there's an error, return sign-up error to front
         return {"Sign-up": "ERROR"}
 
 
 # * Endpoint for deleting a user
 @app.delete("/delete-user")
 def delete_user(delete: Delete):
-    # Checking if the provided username matches any user in the data
-    for id, user in data.items():
-        if user["username"] == delete.username:
-            # if user is the test user, don't delete it
-            if id == 0:
-                break
-            # If a match is found, delete the user
-            del data[id]
+    try:
+        # Checking if the provided username matches any user in the data
+        for id, user in data.items():
+            if user["username"] == delete.username:
+                # if user is the test user, don't delete it
+                if id == 0:
+                    break
+                # If a match is found, delete the user
+                del data[id]
 
-            # Save the changed data to the database
-            save_data(data)
+                # Save the changed data to the database
+                save_data(data)
 
-            # Return success
-            return {"Delete": "SUCCESS"}
+                # Return success
+                return {"Delete": "SUCCESS"}
 
-    # If no match is found, return failed delete
-    return {"Delete": "FAILED", "Info": "bad username"}
+        # If no match is found, return failed delete
+        return {"Delete": "FAILED", "Info": "bad username"}
+
+    # Print out the issue and return error to front
+    except Exception as e:
+        # prints the error out
+        print({"Delete ERROR": f"{e}"})
+        #If there's an error, return delete error to front
+        return {"Delete": "ERROR"}
 
 
 # * Endpoint for getting all data (just for test)
@@ -128,7 +150,7 @@ def load_data_on_startup():
     data = load_data()
 
 
-#! ------------------- network unrelated functions (reallocate later)
+#! ------------------------- network unrelated functions
 
 
 # * Function takes in a username, checks whether it already exists
