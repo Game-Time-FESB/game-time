652cb1e9eec6cb3a768974a5aa69634554fdc45c
Martin <93053179+mjospovich@users.noreply.github.com>
2024-01-06 16:26:51 +0100

    added run bat for new api

---
+++ Backend/runLeagueApi.bat
@@ -0,0 +1,8 @@
+@echo off
+:: To start it just right click on 'run.bat' and click 'Run Code'
+:: Runs the server in seperate terminal window
+:: Ctrl+c to quit the server
+:: 
+::
+:: api startup command, logs into terminal for now
+start cmd /k uvicorn leagueApi:app --host localhost --port 8100 --reload 2>&1
