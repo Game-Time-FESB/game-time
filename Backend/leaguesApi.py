dd5b69f39725a7d00b28e45092cb1124b8872b3b
Martin <93053179+mjospovich@users.noreply.github.com>
2024-01-07 14:47:30 +0100

    added and tested function for adding new teams

--- Backend/leaguesApi.py
+++ Backend/leaguesApi.py
@@ -22,13 +22,22 @@ app.add_middleware(
 )
 
 
-# * User class definition with BaseModel
+# * New league class definition with BaseModel
 class NewLeague(BaseModel):
     league_name: str  # Name of the league
     country: str  # Country league's played in
     city: str  # City where league's played in
     admins: dict  # Admins for league dict(id : username)
     description: Optional[str]  # Description of the league
+
+
+# * New team class definition with BaseModel
+class NewTeam(BaseModel):
+    league_in: str  # Name of the league team's in
+    city_in: str  #  Name of the city team's in
+    team_name: str  # Name of the team
+    players: dict  # Players on the team ({"name" : dict})
+    description: Optional[str]  # Description of the team
 
 
 # * Main dictionary used for temporary storage
@@ -75,6 +84,57 @@ def create_league(new_league: NewLeague):
 
     # Return a success message
     return {"Create League": "SUCCESS", "Info": "League successfully created"}
+
+
+# * Endpoint for adding a new team to a league
+@app.post("/add-team")
+def add_team(new_team: NewTeam):
+    # Convert the NewTeam object to a dictionary
+    team_dict = new_team.dict()
+
+    # Define the filename based on the city
+    filename = f"Database/{(new_team.city_in).lower()}_leagues.json"
+
+    # Initialize an empty dictionary for data
+    data = {}
+
+    # Check if the file exists
+    if os.path.exists(filename):
+        # Load the data from the file
+        with open(filename, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        # Check if the league's name exists
+    if new_team.league_in in data.keys():
+        # Check if the 'teams' key exists
+        if "teams" in data[new_team.league_in].keys():
+            # Check if the team's name already exists
+            if new_team.team_name in data[new_team.league_in]["teams"].keys():
+                return {"Add Team": "FAILED", "Info": "Team already exists"}
+
+            # Use the team_name as the key and store the team_dict in league's 'teams' data
+            data[new_team.league_in]["teams"][new_team.team_name] = team_dict
+
+            # Store the data back to the file
+            with open(filename, "w", encoding="utf-8") as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+
+            # Clear the temp dict
+            data = {}
+
+            # Return a success message
+            return {
+                "Add Team": "SUCCESS",
+                "Info": "Team successfully added to the league",
+            }
+
+        else:
+            # If the 'teams' key doesn't exist, return an error message
+            return {"Add Team": "ERROR", "Info": "'teams' key not found in the league"}
+
+    else:
+        # If the league doesn't exist, return an error message
+        return {"Add Team": "ERROR", "Info": "League not found"}
 
 
 # * Endpoint for getting all data (just for test)
