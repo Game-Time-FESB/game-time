2dc226d77811467fc4175034cfba54f1a78197f5
Luka <106626899+ldoric@users.noreply.github.com>
2024-01-09 23:16:18 +0100

    ups

--- Web/js/script.js
+++ Web/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //url - ask pb and change the second input variable
-sessionStorage.setItem('urlPB', '93.140.173.179');
+sessionStorage.setItem('urlPB', '9');
 sessionStorage.setItem('portPB', '6969');
 
 // test global variabli
